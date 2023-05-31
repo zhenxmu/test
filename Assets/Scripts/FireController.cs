22c1eedc7671f3db9e6359073c70662d1db08d39
1269716935 <73485509+1269716935@users.noreply.github.com>
2023-05-31 09:52:05 +0800

    Boss

---
+++ Assets/Scripts/FireController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject fireballPrefab; // 火球预制体
+    public float fireballSpeed = 5f; // 火球速度
+    public float fireballInterval = 3f; // 发射火球的间隔时间
+    private float lastFireballTime = 0f; // 上一次发射火球的时间
+    public int health;
+    public int damage;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.SetBool("Attack", false);
+        // 检查是否到了发射火球的时间
+        if (Time.time - lastFireballTime > fireballInterval)
+        {
+            animator.SetBool("Attack", true);
+            // 创建火球实例
+            GameObject fireball = Instantiate(fireballPrefab, transform.position, Quaternion.identity);
+            // 设置火球速度
+            fireball.GetComponent<Rigidbody2D>().velocity = new Vector2(-fireballSpeed, 0);
+            // 更新上一次发射火球的时间
+            lastFireballTime = Time.time;
+
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        //死亡判断
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
+
